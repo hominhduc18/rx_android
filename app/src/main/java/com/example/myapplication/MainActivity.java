092bcfa04a9211e6345d7e29e92554b93a4db98e
hominhduc18 <hoduc092@gmail.com>
2023-10-20 14:22:10 +0700

    rx_with_retrofit_all

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,10 +6,13 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -30,6 +33,11 @@ public class MainActivity extends AppCompatActivity {
     private DataAdapter mDataAdapter;
     private RecyclerView rcvData;
 
+    private List<ObjectData> mListData;
+
+    private  Disposable disposable;
+
+    private ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +52,10 @@ public class MainActivity extends AppCompatActivity {
         DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
         rcvData.addItemDecoration(dividerItemDecoration);
 
+        mListData = new ArrayList<>();
+
+        progressDialog = new ProgressDialog(this);
+
         btncallApi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,21 +65,57 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @SuppressLint("CheckResult")
     private void onclickCallApi(){
-        mDataAdapter = new DataAdapter(getListData());
-        rcvData.setAdapter(mDataAdapter);
+//        mDataAdapter = new DataAdapter(getListData());
+//        rcvData.setAdapter(mDataAdapter);
+
+        progressDialog.show();
+        APIService.apiservice.callApi()
+                .observeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<List<ObjectData>>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+                        disposable = d;
+                    }
+                    @Override
+                    public void onNext(@NonNull List<ObjectData> objectData) {
+                            mListData = objectData;
+                    }
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        Toast.makeText(MainActivity.this, "Call API ERRO",Toast.LENGTH_SHORT).show();
+                    }
+                    @Override
+                    public void onComplete() {
+                        Toast.makeText(MainActivity.this, "Call API SUCCESSS",Toast.LENGTH_SHORT).show();
+
+                        mDataAdapter = new DataAdapter(mListData);
+                        rcvData.setAdapter(mDataAdapter);
+
+                        progressDialog.dismiss();
+                    }
+                });
     }
-    private List<ObjectData> getListData(){
-        List<ObjectData> list = new ArrayList<>();
 
-        list.add(new ObjectData("title 1", "body 1"));
-        list.add(new ObjectData("title 2", "body 2"));
-        list.add(new ObjectData("title 3", "body 3"));
-        list.add(new ObjectData("title 4", "body 4"));
-        list.add(new ObjectData("title 5", "body 5"));
-
-
-
-        return list;
-    }
+  protected void onDestroy(){
+        super.onDestroy();
+        if(disposable !=null){
+            disposable.dispose();
+        }
+  }
+    //    private List<ObjectData> getListData(){
+//        List<ObjectData> list = new ArrayList<>();
+//
+//        list.add(new ObjectData("title 1", "body 1"));
+//        list.add(new ObjectData("title 2", "body 2"));
+//        list.add(new ObjectData("title 3", "body 3"));
+//        list.add(new ObjectData("title 4", "body 4"));
+//        list.add(new ObjectData("title 5", "body 5"));
+//
+//
+//
+//        return list;
+//    }
 }
